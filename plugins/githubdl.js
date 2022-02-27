db3957c0e451108451376b7d3645af2cdaa0bb8d
LeonGantengz <100405315+LeonGantengz@users.noreply.github.com>
2022-02-27 20:29:56 +0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,6 +1,6 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw 'usernamenya mana om\nContoh saipulanuar'
+if (!args[0]) throw 'usernamenya mana om\nContoh Bl4ck-lion'
 if (!args[1]) throw 'repo nya mana?\nContoh v18'
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
